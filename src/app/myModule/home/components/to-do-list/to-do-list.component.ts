2d74047490c50b93407220c1324cf4ca6d2d6c2c
Cronos Uchoa <Gabriel5432@gmail.com>
2022-07-03 14:38:38 -0300

    Criando funcionalidades - step 2
    Signed-off-by: Cronos Uchoa <Gabriel5432@gmail.com>

--- src/app/myModule/home/components/to-do-list/to-do-list.component.ts
+++ src/app/myModule/home/components/to-do-list/to-do-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,DoCheck } from '@angular/core';
 //interface
 import { TaskList } from '../../model/task-list';
 
@@ -6,16 +6,18 @@ import { TaskList } from '../../model/task-list';
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.scss']
-})
-export class ToDoListComponent implements OnInit {
+}) 
+export class ToDoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [
     
   ];
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ ngDoCheck(){
+  //organiza os itens. quando o item estiver marcado ele vai pro final
+  this.taskList.sort((first,last) => Number(first.checked) - Number(last.checked));
+ }
 
   public setEmitTaskList(event:string){
     this.taskList.push({task:event, checked:false})
@@ -29,6 +31,15 @@ export class ToDoListComponent implements OnInit {
     const confim = window.confirm("Você tem certeza !?")
     if(confim){
       this.taskList = [];
+    }
+  }
+
+  public validationInput(event:string, index:number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja deletar?")
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
     }
   }
 }
